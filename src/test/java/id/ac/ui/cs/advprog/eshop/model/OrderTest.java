7272a9ce2e0f732149008e52a624c15142c8f48c
rafasoelistiono <rafasoelistiono@gmail.com>
2026-03-09 13:20:08 +0700

    [GREEN] Implement Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import org.apache.catalina.servlets.DefaultServlet.SortManager.Order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
